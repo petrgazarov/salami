2aef948288e28c2025519ff10f38e1de7a05c08b
Petr Gazarov <petrgazarov@gmail.com>
2023-09-25 18:25:19 -0700

    Add test coverage command; GetFilesFromObjects func

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ install:
 
 test: # Resolves all test files ending in _test.go and runs them
 	find . -name "*_test.go" -print0 | xargs -0 -n1 dirname | sort -u | xargs -L1 go test
+
+test_coverage:
+	go test ./... -coverprofile=coverage.out
--- backend/target/terraform/get_files_from_objects.go
+++ backend/target/terraform/get_files_from_objects.go
@@ -1,9 +1,39 @@
 package terraform
 
-import "salami/common/types"
+import (
+	"salami/common/types"
+	"strings"
+)
 
-func GetFilesFromObjects([]*types.Object) []*types.TargetFile {
-	// All resources, variables and outputs follow the same file/folder structure as the source files.
+func GetFilesFromObjects(objects []*types.Object) []*types.TargetFile {
+	targetFiles := []*types.TargetFile{}
 
-	return nil
+	var currentTargetFile *types.TargetFile
+	var lastSourceFilePath string
+	for _, object := range objects {
+		currentContent := ""
+		for _, codeSegment := range object.CodeSegments {
+			currentContent += codeSegment.Content
+		}
+
+		if object.GetSourceFilePath() != lastSourceFilePath {
+			currentTargetFile = &types.TargetFile{
+				FilePath: getTargetFilePath(object.GetSourceFilePath()),
+				Content:  currentContent,
+			}
+			targetFiles = append(targetFiles, currentTargetFile)
+			lastSourceFilePath = object.GetSourceFilePath()
+		} else {
+			currentTargetFile.Content += ("\n\n" + currentContent)
+		}
+	}
+
+	return targetFiles
+}
+
+func getTargetFilePath(sourceFilePath string) string {
+	if strings.HasSuffix(sourceFilePath, ".sami") {
+		return strings.TrimSuffix(sourceFilePath, ".sami") + ".tf"
+	}
+	return sourceFilePath
 }
