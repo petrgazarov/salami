29cccf780e6153f647efb96ac8736016460d1ff9
Petr Gazarov <petrgazarov@gmail.com>
2023-10-06 16:04:54 -0700

    Example: update cloudtrail name

--- examples/public_and_private_ecs_services/iac/cloudtrail.tf
+++ examples/public_and_private_ecs_services/iac/cloudtrail.tf
@@ -38,7 +38,7 @@ POLICY
 }
 
 resource "aws_cloudtrail" "Cloudtrail" {
-  name                          = "cloudtrail"
+  name                          = "salami-cloudtrail"
   s3_bucket_name                = aws_s3_bucket.SalamiCloudtrailLogsBucket.bucket
   include_global_service_events = true
   is_multi_region_trail         = true
