dbc65aa44dd3e17fd4f4f02cd3b053da49c3dbae
Petr Gazarov <petrgazarov@gmail.com>
2023-10-18 05:41:36 -0700

    GA: debug

--- .github/scripts/verify-compile/run.sh
+++ .github/scripts/verify-compile/run.sh
@@ -18,5 +18,12 @@ fi
 
 if ! cmp -s terraform/s3_bucket.tf ${SALAMI_REPO_DIRECTORY}/.github/scripts/verify-compile/s3_bucket.tf; then
   echo "Error: terraform/s3_bucket.tf and ${SALAMI_REPO_DIRECTORY}/.github/scripts/verify-compile/s3_bucket.tf are not the same"
+  
+  echo "Contents of terraform/s3_bucket.tf:"
+  cat terraform/s3_bucket.tf
+
+  echo "Contents of ${SALAMI_REPO_DIRECTORY}/.github/scripts/verify-compile/s3_bucket.tf:"
+  cat ${SALAMI_REPO_DIRECTORY}/.github/scripts/verify-compile/s3_bucket.tf
+
   exit 1
 fi
