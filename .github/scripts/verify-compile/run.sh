1f7cb73d66cab781fb6e91434e6344f7613154e9
Petr Gazarov <petrgazarov@gmail.com>
2023-10-18 05:35:57 -0700

    GA: correct paths

--- .github/scripts/verify-compile/run.sh
+++ .github/scripts/verify-compile/run.sh
@@ -6,8 +6,8 @@ SALAMI_REPO_DIRECTORY=$1
 
 mkdir salami
 mkdir terraform
-cp ${SALAMI_REPO_DIRECTORY}/.github/fixtures/s3_bucket.sami salami/s3_bucket.sami
-cp ${SALAMI_REPO_DIRECTORY}/.github/fixtures/salami.yaml salami.yaml
+cp ${SALAMI_REPO_DIRECTORY}/.github/scripts/verify-compile/s3_bucket.sami salami/s3_bucket.sami
+cp ${SALAMI_REPO_DIRECTORY}/.github/scripts/verify-compile/salami.yaml salami.yaml
 
 salami compile
 
@@ -16,7 +16,7 @@ if [[ ! -f terraform/s3_bucket.tf ]]; then
   exit 1
 fi
 
-if ! cmp -s terraform/s3_bucket.tf ${SALAMI_REPO_DIRECTORY}/.github/fixtures/s3_bucket.tf; then
-  echo "Error: terraform/s3_bucket.tf and ${SALAMI_REPO_DIRECTORY}/.github/fixtures/s3_bucket.tf are not the same"
+if ! cmp -s terraform/s3_bucket.tf ${SALAMI_REPO_DIRECTORY}/.github/scripts/verify-compile/s3_bucket.tf; then
+  echo "Error: terraform/s3_bucket.tf and ${SALAMI_REPO_DIRECTORY}/.github/scripts/verify-compile/s3_bucket.tf are not the same"
   exit 1
 fi
